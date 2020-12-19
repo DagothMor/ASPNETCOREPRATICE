3d3eb1535fc120e34738b49faaafa3a85d59b6f7
DagothMor <siegfried1997@gmail.com>
2020-12-19 19:48:14 +0300

    Edited all services through UoW Edited AddAutomapper in startup

--- ASPNETHomework/ASPNETHomework.Services/Services/OrderService.cs
+++ ASPNETHomework/ASPNETHomework.Services/Services/OrderService.cs
@@ -7,6 +7,7 @@ using ASPNETHomework.Models.DTO;
 using ASPNETHomework.Repositories.Interfaces;
 using ASPNETHomework.Services.Interfaces;
 using ASPNETHomework.Repositories.Interfaces.CRUD;
+using ASPNETHomework.DAL;
 
 namespace ASPNETHomework.Services.Services
 {
@@ -15,45 +16,45 @@ namespace ASPNETHomework.Services.Services
 	/// </summary>
 	public class OrderService : IOrderService
 	{
-		private readonly IOrderRepository _repository;
+		private readonly UnitOfWork _unitOfWork;
 
 		/// <summary>
 		/// Initialize an instance <see cref="OrderService"/>.
 		/// </summary>
-		/// <param name="repository">Repository.</param>
-		public OrderService(IOrderRepository repository)
+		/// <param name="UnitOfWork">UnitOfWork.</param>
+		public OrderService(UnitOfWork unitOfWork)
 		{
-			_repository = repository;
+			_unitOfWork = unitOfWork;
 		}
 
 		///<inheritdoc cref="ICreatable{TDto}.CreateAsync(TDto)"/>
 		public async Task<OrderDto> CreateAsync(OrderDto dto)
 		{
-			return await _repository.CreateAsync(dto);
+			return await _unitOfWork.Orders.CreateAsync(dto);
 		}
 
 		/// <inheritdoc cref="IDeletable.DeleteAsync(int[])"/>
 		public async Task DeleteAsync(params int[] ids)
 		{
-			await _repository.DeleteAsync(ids);
+			await _unitOfWork.Orders.DeleteAsync(ids);
 		}
 
 		/// <inheritdoc cref="IGettableById{TDto}.GetAsync(int, CancellationToken)"/>
 		public async Task<OrderDto> GetAsync(int id, CancellationToken token = default)
 		{
-			return await _repository.GetAsync(id);
+			return await _unitOfWork.Orders.GetAsync(id);
 		}
 
 		/// <inheritdoc cref="IGettable{TDto}.GetAsync(CancellationToken)"/>
 		public async Task<IEnumerable<OrderDto>> GetAsync(CancellationToken token = default)
 		{
-			return await _repository.GetAsync(token);
+			return await _unitOfWork.Orders.GetAsync(token);
 		}
 
 		/// <inheritdoc cref="IUpdatable{TDto}.UpdateAsync(TDto)"/>
 		public async Task<OrderDto> UpdateAsync(OrderDto dto)
 		{
-			return await _repository.UpdateAsync(dto);
+			return await _unitOfWork.Orders.UpdateAsync(dto);
 		}
 	}
 }
