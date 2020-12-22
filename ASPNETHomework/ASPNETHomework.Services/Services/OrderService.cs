777a463995c1622f8c3583934ca9e4272451613f
DagothMor <siegfried1997@gmail.com>
2020-12-22 14:53:05 +0300

    Hot Fix

--- ASPNETHomework/ASPNETHomework.Services/Services/OrderService.cs
+++ ASPNETHomework/ASPNETHomework.Services/Services/OrderService.cs
@@ -8,6 +8,7 @@ using ASPNETHomework.Repositories.Interfaces;
 using ASPNETHomework.Services.Interfaces;
 using ASPNETHomework.Repositories.Interfaces.CRUD;
 using ASPNETHomework.DAL;
+using System.Linq;
 
 namespace ASPNETHomework.Services.Services
 {
@@ -30,7 +31,34 @@ namespace ASPNETHomework.Services.Services
 		///<inheritdoc cref="ICreatable{TDto}.CreateAsync(TDto)"/>
 		public async Task<OrderDto> CreateAsync(OrderDto dto)
 		{
-			return await _unitOfWork.Orders.CreateAsync(dto);
+			using var scope = await _unitOfWork.Orders.Context.Database.BeginTransactionAsync();
+			try
+			{
+				if (dto.Products.Sum(x=>x.Price)>dto.Customer.Money)
+				{
+					throw new Exception("Not enough money");
+				}
+				else
+				{
+					//TODO:определиться уже наконец с типом данных денег
+					dto.Customer.Money -= (float)dto.Products.Sum(x => x.Price);
+					var customer = new CustomerDto();
+					customer.Id = dto.Customer.Id;
+					customer.FirstName = dto.Customer.FirstName;
+					customer.LastName = dto.Customer.LastName;
+					customer.Money = dto.Customer.Money;
+					customer.City = dto.Customer.City;
+					await _unitOfWork.Customers.UpdateAsync(customer);
+					var order = await _unitOfWork.Orders.CreateAsync(dto);
+					scope.Commit();
+					return order;
+				}
+			}
+			catch (Exception ex)
+			{
+				scope.Rollback();
+				throw ex;
+			}
 		}
 
 		/// <inheritdoc cref="IDeletable.DeleteAsync(int[])"/>
